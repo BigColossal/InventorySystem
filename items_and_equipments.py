2ce4bed92770fcb2353863df3f9a2d3194230e10
Jeremy Perez <jeremyperez1409@gmail.com>
2026-03-24 09:36:22 -0400

    feat: modularized the code into separate files as main.py was getting too large. added more functionality within inventory object

---
+++ items_and_equipments.py
@@ -0,0 +1,75 @@
+from enum import Enum
+
+class equipmentTypes(Enum):
+    Helmet = 1
+    Chestplate = 2
+    Leggings = 3
+    RightGlove = 4
+    LeftGlove = 5
+    Boots = 6
+    Sword = 7
+    LongSword = 8
+    DualBlades = 9
+    Spear = 10
+    Bow = 11
+    Wand = 12
+    Accessory = 13
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+class Equipment(Item):
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.type = type
+        self.equipped = False
+
+equipmentStats = {
+    equipmentTypes.Helmet: {
+
+    },
+    equipmentTypes.Chestplate: {
+
+    },
+    equipmentTypes.RightGlove: {
+
+    },
+    equipmentTypes.LeftGlove: {
+
+    },
+    equipmentTypes.Leggings: {
+
+    },
+    equipmentTypes.Boots: {
+
+    },
+    equipmentTypes.Sword: {
+
+    },
+    equipmentTypes.DualBlades: {
+
+    },
+    equipmentTypes.Spear: {
+
+    },
+    equipmentTypes.LongSword: {
+
+    },
+    equipmentTypes.Bow: {
+
+    },
+    equipmentTypes.Wand: {
+
+    },
+    equipmentTypes.Accessory: {
+
+    },
+}
+
+itemDescriptions = {
+    
+}
+
+def getEquipmentStats(equipment):
+    pass
